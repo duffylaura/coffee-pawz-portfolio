351066215e67f6795599e4cde8939cd6c7184b67
Coffee Pawz <lauralynneduffy@gmail.com>
2022-12-21 16:27:03 -0500

    The About Me section contains an avatar of the developer and a short bio about them.

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -2,22 +2,31 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
 import Project from '../components/project';
+import MyOctocat from '../assets/octocat.png';
+import Image from 'react-bootstrap/Image'
 
 function Homepage() {
   return (
     <>
     <Container id="About">
       <br />
-      <Row>
+      <Row className="justify-content-md-center">
         <h1> About Me </h1>
-        <Card>
-            <Card.Body>
-              Hello! My name is Laura Duffy. <br/>
-              Welcome to my portfolio page! <br/>
-              I am a software developer who is completing UC Berkeley's Coding Bootcamp! 
-            </Card.Body>
-        </Card>
+        <Col xs lg="2">
+            <Image src={MyOctocat} alt="Custom octocat" 
+            className = {{ float: 'right'}}
+            style={{ width: '18rem' }} />
+        </Col>
+        <Col md="auto">
+        <p>   
+          Hello! My name is Laura Duffy. <br/>
+          Welcome to my portfolio page! <br/>
+          I am a software developer who is completing UC Berkeley's Coding Bootcamp! 
+        </p>
+        </Col>
+        <Col xs lg="2"> </Col>
       </Row>
       <br /> 
     </Container>
