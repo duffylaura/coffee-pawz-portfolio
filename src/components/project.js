0210b2d2ab86a1a74565330ffe12eee5c22ed992
Coffee Pawz <lauralynneduffy@gmail.com>
2022-12-21 17:49:40 -0500

    Project cards logic done. Need to fix images (not displaying)

--- src/components/project.js
+++ src/components/project.js
@@ -1,24 +1,30 @@
 import React from 'react';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
-const placeholder = require('../assets/red-smoke.jpg');
+//const placeholder = require('../assets/red-smoke.jpg');
 
-function Project() {
+function Project(props) {
 
 return (
-    <Col> 
+  <div className="project-card">  
+  {/* Here we map over each grocery item and return a new array of `li` elements that contains the grocery name */}
+  {/* When using map you must provide a unique key attribute to each item. Ours is `item.id` */}
+  {props.proj.map(item => (
+    <Col key={item.id}> 
     <Card>
-      <Card.Img variant="top" src= {placeholder} />
+      <Card.Img variant="top" src= {item.img} alt="clipart"/>
         <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
-            </Card.Text>
+          <Card.Title>{item.title}</Card.Title>
+          <Card.Text> {item.about}</Card.Text>
+          <Card.Link href= {item.deploy}>Deployed Link</Card.Link>
+          <Card.Link href={item.repo}>Repository Link</Card.Link>
       </Card.Body>
     </Card>
     </Col>
-)
-};
+  ))}
+
+</div>
+
+)};
 
 export default Project; 
--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -7,6 +7,54 @@ import Project from '../components/project';
 import MyOctocat from '../assets/octocat.png';
 import Image from 'react-bootstrap/Image'
 
+const proj1 = [
+  {id: 1, 
+  title: 'Music Social Media Site', 
+  about: 'Did you listen to a song today that made you feel a certain way, but want to express it in a new and fun way? Do you want to join a community of like-minded music lovers? If yes, Then Purple wAIve is perfect for you! Purple wAIve is a webpage built for audiophiles, artists, writers or really anyone who likes music and artistic expression. Purple wAIve uses DALLE2s artificial intelligence system to generate pictures based on user input.The AI generated images offer an expansion of the scope of artistic expression since most humans would not think to create an image/picture the same as an AI system that has millions and millions of pictures/images to reference all at a moments notice.',
+  repo: 'https://github.com/duffylaura/purple_wAIve',
+  deploy: 'https://purple-waive.herokuapp.com/',
+  img: '../assets/purple-waive.png',
+  },
+  {id: 2, 
+    title: 'Particle Problems', 
+    about: 'Particle Problems is a reliable, easy-to-use app that tells the user the pollen count and the air quality in any location they choose to search. Particle Problems makes use of the Breeze-O-Meter and Open Weather Map APIs as well as the Bulma, Leaflet, jQuery, and jQuery widgets libraries. <br/> Our application will provide a solution to those with Particle Problems in one click, instead of several Googles per day.',
+    repo: 'https://github.com/duffylaura/Particle-Problems',
+    deploy: 'https://duffylaura.github.io/Particle-Problems/',
+    img: '../assets/particle-problems.png',
+  },
+  {id: 3, 
+    title: 'Coffee Pawz Note Taker App', 
+    about: 'Do you ever have an amazing coffee thought!? That feeling when you are spacing out while sipping on a cup of coffee and then think "Oh! I should write that down!” BUT WHERE!? The Note Take app offers the perfect solution! The Note Taker application can be used to write and save notes. Using an Express.js back end, this app will save and retrieve your stored notes (each having a unique ID!) from a JSON file. The application has been deployed using Heroku.',
+    repo: 'https://github.com/duffylaura/note-taker-app',
+    deploy: 'https://coffee-pawz-note-taker-app.herokuapp.com/',
+    img: '../assets/note-taker.png',
+  },
+];
+
+const proj2 = [
+  {id: 4, 
+    title: 'Weather Tracker App', 
+    about: 'Get ready to be BLOWN away! (And for some weather puns!) This weather web application uses the third party API, OpenWeather, to retrieve weather data for a city input by the user. After inputting their chosen city and clicking the search button, the user will see the current weather temperature, humidity, windspeed and an descriptive icon. They will also see the temperature, humidity, windspeed, and icon for the next five days. The weather app runs in the browser and features dynamically updated HTML and CSS. This application also uses local storage to store each city a user searches for. The application takes each searched city and adds it to an array stored in local storage. The storage is displayed in the form of dynamically generated, clickable buttons on the page.',
+    repo: 'https://github.com/duffylaura/weather-challenge',
+    deploy: 'https://duffylaura.github.io/weather-challenge/',
+    img: '../assets/weather.png',
+  },
+  {id: 5, 
+    title: 'Password Generator', 
+    about: 'The purpose of this repository is to refactor code for a Password Generator. The scenario given was that this is a password generator used by employees with access to sensitive data. The application must be able to generate passwords that meet certain criteria input by the user. The purpose of this application is to create strong passwords to provide greater security.',
+    repo: 'https://github.com/duffylaura/Password-Generator',
+    deploy: 'https://duffylaura.github.io/Password-Generator/',
+    img: '../assets/fox-hacker.png',
+  },
+  {id: 6, 
+    title: 'Daily Planner', 
+    about: 'This web application was designed for people with busy schedules! It allows the user to add events to a daily planner so that they can manage their schedule for the day effectively. While working on this project, the coder developed skills in using the Moment.js library to work with date and time, Bootstrap, and local storage.',
+    repo: 'https://github.com/duffylaura/daily-planner',
+    deploy: 'https://duffylaura.github.io/daily-planner/ ',
+    img: '../assets/planner.png',
+  },
+];
+
 function Homepage() {
   return (
     <>
@@ -34,15 +82,11 @@ function Homepage() {
     <Container id="Portfolio">
       <h1> Portfolio </h1>
       <Row>
-        <Project />
-        <Project />
-        <Project />
+        <Project proj = {proj1} />
       </Row>
       <br />
       <Row>
-        <Project />
-        <Project />
-        <Project />
+        <Project proj = {proj2} />
       </Row>
     <br />
     </Container>
