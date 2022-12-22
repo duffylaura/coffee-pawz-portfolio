9dea81daf7dc255740f7cb5c3d14fabb6f3301cb
Coffee Pawz <lauralynneduffy@gmail.com>
2022-12-21 19:54:07 -0500

    Making portfolio cards

--- src/components/project.js
+++ src/components/project.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
 //const placeholder = require('../assets/red-smoke.jpg');
 
 function Project(props) {
 
 return (
+  <Row>
   <div className="project-card">  
   {/* Here we map over each grocery item and return a new array of `li` elements that contains the grocery name */}
   {/* When using map you must provide a unique key attribute to each item. Ours is `item.id` */}
-  {props.proj.map(item => (
-    <Col key={item.id}> 
-    <Card>
-      <Card.Img variant="top" src= {item.img} alt="clipart"/>
+  {props.proj.map(item => ( 
+    <Col key={item.id} >
+    <Card >
+      <Card.Img variant="top" src= {`../img/${item.img}.png`} alt="clipart" style={{ width: '18rem' }}/>
         <Card.Body>
           <Card.Title>{item.title}</Card.Title>
           <Card.Text> {item.about}</Card.Text>
@@ -22,8 +24,8 @@ return (
     </Card>
     </Col>
   ))}
-
 </div>
+</Row>
 
 )};
 
--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -13,21 +13,21 @@ const proj1 = [
   about: 'Did you listen to a song today that made you feel a certain way, but want to express it in a new and fun way? Do you want to join a community of like-minded music lovers? If yes, Then Purple wAIve is perfect for you! Purple wAIve is a webpage built for audiophiles, artists, writers or really anyone who likes music and artistic expression. Purple wAIve uses DALLE2s artificial intelligence system to generate pictures based on user input.The AI generated images offer an expansion of the scope of artistic expression since most humans would not think to create an image/picture the same as an AI system that has millions and millions of pictures/images to reference all at a moments notice.',
   repo: 'https://github.com/duffylaura/purple_wAIve',
   deploy: 'https://purple-waive.herokuapp.com/',
-  img: '../assets/purple-waive.png',
+  img: 'purple-waive',
   },
   {id: 2, 
-    title: 'Particle Problems', 
-    about: 'Particle Problems is a reliable, easy-to-use app that tells the user the pollen count and the air quality in any location they choose to search. Particle Problems makes use of the Breeze-O-Meter and Open Weather Map APIs as well as the Bulma, Leaflet, jQuery, and jQuery widgets libraries. <br/> Our application will provide a solution to those with Particle Problems in one click, instead of several Googles per day.',
-    repo: 'https://github.com/duffylaura/Particle-Problems',
-    deploy: 'https://duffylaura.github.io/Particle-Problems/',
-    img: '../assets/particle-problems.png',
+    title: 'Password Generator', 
+    about: 'The purpose of this repository is to refactor code for a Password Generator. The scenario given was that this is a password generator used by employees with access to sensitive data. The application must be able to generate passwords that meet certain criteria input by the user. The purpose of this application is to create strong passwords to provide greater security.',
+    repo: 'https://github.com/duffylaura/Password-Generator',
+    deploy: 'https://duffylaura.github.io/Password-Generator/',
+    img: 'fox-hacker',
   },
   {id: 3, 
     title: 'Coffee Pawz Note Taker App', 
     about: 'Do you ever have an amazing coffee thought!? That feeling when you are spacing out while sipping on a cup of coffee and then think "Oh! I should write that down!” BUT WHERE!? The Note Take app offers the perfect solution! The Note Taker application can be used to write and save notes. Using an Express.js back end, this app will save and retrieve your stored notes (each having a unique ID!) from a JSON file. The application has been deployed using Heroku.',
     repo: 'https://github.com/duffylaura/note-taker-app',
     deploy: 'https://coffee-pawz-note-taker-app.herokuapp.com/',
-    img: '../assets/note-taker.png',
+    img: 'note-taker',
   },
 ];
 
@@ -37,58 +37,51 @@ const proj2 = [
     about: 'Get ready to be BLOWN away! (And for some weather puns!) This weather web application uses the third party API, OpenWeather, to retrieve weather data for a city input by the user. After inputting their chosen city and clicking the search button, the user will see the current weather temperature, humidity, windspeed and an descriptive icon. They will also see the temperature, humidity, windspeed, and icon for the next five days. The weather app runs in the browser and features dynamically updated HTML and CSS. This application also uses local storage to store each city a user searches for. The application takes each searched city and adds it to an array stored in local storage. The storage is displayed in the form of dynamically generated, clickable buttons on the page.',
     repo: 'https://github.com/duffylaura/weather-challenge',
     deploy: 'https://duffylaura.github.io/weather-challenge/',
-    img: '../assets/weather.png',
+    img: 'weather',
   },
   {id: 5, 
-    title: 'Password Generator', 
-    about: 'The purpose of this repository is to refactor code for a Password Generator. The scenario given was that this is a password generator used by employees with access to sensitive data. The application must be able to generate passwords that meet certain criteria input by the user. The purpose of this application is to create strong passwords to provide greater security.',
-    repo: 'https://github.com/duffylaura/Password-Generator',
-    deploy: 'https://duffylaura.github.io/Password-Generator/',
-    img: '../assets/fox-hacker.png',
+    title: 'Particle Problems', 
+    about: 'Particle Problems is a reliable, easy-to-use app that tells the user the pollen count and the air quality in any location they choose to search. Particle Problems makes use of the Breeze-O-Meter and Open Weather Map APIs as well as the Bulma, Leaflet, jQuery, and jQuery widgets libraries. <br/> Our application will provide a solution to those with Particle Problems in one click, instead of several Googles per day.',
+    repo: 'https://github.com/duffylaura/Particle-Problems',
+    deploy: 'https://duffylaura.github.io/Particle-Problems/',
+    img: 'particle-solutions',
   },
   {id: 6, 
     title: 'Daily Planner', 
     about: 'This web application was designed for people with busy schedules! It allows the user to add events to a daily planner so that they can manage their schedule for the day effectively. While working on this project, the coder developed skills in using the Moment.js library to work with date and time, Bootstrap, and local storage.',
     repo: 'https://github.com/duffylaura/daily-planner',
     deploy: 'https://duffylaura.github.io/daily-planner/ ',
-    img: '../assets/planner.png',
+    img: 'planner',
   },
 ];
 
 function Homepage() {
   return (
-    <>
+  <>
     <Container id="About">
       <br />
       <Row className="justify-content-md-center">
         <h1> About Me </h1>
-        <Col xs lg="2">
+        <Col s>
             <Image src={MyOctocat} alt="Custom octocat" 
             className = {{ float: 'right'}}
             style={{ width: '18rem' }} />
         </Col>
-        <Col md="auto">
-        <p>   
-          Hello! My name is Laura Duffy. <br/>
-          Welcome to my portfolio page! <br/>
-          I am a software developer who is completing UC Berkeley's Coding Bootcamp! 
-        </p>
+        <Col lg>
+            <br />
+            <p>   
+              Hello! My name is Laura Duffy. Welcome to my portfolio page! I am a software developer who is completing UC Berkeley's Coding Bootcamp! 
+            </p>
         </Col>
-        <Col xs lg="2"> </Col>
+        <Col s> </Col>
       </Row>
       <br /> 
     </Container>
-
+    
     <Container id="Portfolio">
       <h1> Portfolio </h1>
-      <Row>
         <Project proj = {proj1} />
-      </Row>
-      <br />
-      <Row>
         <Project proj = {proj2} />
-      </Row>
-    <br />
     </Container>
 
     <Container id="Resume">
@@ -102,9 +95,7 @@ function Homepage() {
         </Card>
       </Row>
       <br /> 
-
     </Container>
-
     <Container id="Contact">
     <br />
       <Row>
@@ -116,9 +107,8 @@ function Homepage() {
         </Card>
       </Row>
       <br /> 
-
     </Container>
-    </>
+  </>
   );
 }
 
