6f5dd39e29aa2b11396e0a83fc65f3bc0b55dc53
Coffee Pawz <lauralynneduffy@gmail.com>
2023-02-06 11:57:48 -0500

    Update contenton About card

--- src/components/about.js
+++ src/components/about.js
@@ -15,9 +15,8 @@ const About = () => {
             <Image src={MyOctocat} alt="Custom octocat" className = "profile-image"/>
             <br/><br/>
             <p> Hello! My name is Laura Duffy and I am a software developer. Welcome to my portfolio page!
-            I am looking for a Software Engineering position and am currently honing my software engineering skills through UC Berkeley's Full Stack Coding Bootcamp.
-            I am changing career directions toward software engineering and product design/management from mathematics education and research.
-            I hold a Professional Master's in Education from Trinity College, Dublin, Ireland, and have 8+ years of professional experience, culminating in a STEM Program Director position this past summer. Additionally, I was a top saleswoman for Croteaux Vineyards from 2015-2020. </p>
+                I am a software engineer specializing in frontend engineering, design, and UX/UI. 
+            </p>
             </Container>
         </Row> <br/> <br/> <br/>
         </Container>
